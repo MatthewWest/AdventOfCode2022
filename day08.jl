715e43c7ecd9266956b6c11c826ff05180b93f65
Matthew West <mattwest@google.com>
2022-12-07 23:47:27 -0700

    Pluto contents

--- day08.jl
+++ day08.jl
@@ -217,6 +217,22 @@ end
 # ╔═╡ eb42457e-5798-414f-8118-ac03fac31f8c
 part2()
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.3"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
 # ╟─11b68808-76b5-11ed-16ba-512914043c0b
 # ╟─4fd0ad54-5907-41ba-ad28-6af65b54f5f2
@@ -229,3 +245,5 @@ part2()
 # ╠═3c81c886-38d4-4f3a-8afb-5e0df12e3180
 # ╠═ef662ac2-fc1c-4c7e-830a-ee4e2a71b646
 # ╠═eb42457e-5798-414f-8118-ac03fac31f8c
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
