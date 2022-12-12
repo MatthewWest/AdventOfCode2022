503d85410f2959a87cac3f1e6673d81a80a6498b
Matthew West <mattwest@google.com>
2022-12-11 23:40:11 -0700

    Part 2 reimplemented using BFS from the end.

--- src/day12.jl
+++ src/day12.jl
@@ -29,7 +29,7 @@ function manhattan_dist(a::CartesianIndex, b::CartesianIndex)::Int
     abs(a[1] - b[1]) + abs(a[2] - b[2])
 end
 
-function neighbors(pos::CartesianIndex, map)
+function neighbors(map, pos::CartesianIndex)
     steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     ns = CartesianIndex[]
     for step ∈ steps
@@ -62,7 +62,7 @@ function a_star(map, h, start::CartesianIndex, dest::CartesianIndex)
         if p == dest
             return construct_path(p, came_from)
         end
-        for n ∈ neighbors(p, map)
+        for n ∈ neighbors(map, p)
             cost = cheapest[p] + 1
             if n ∉ keys(cheapest) || cost < cheapest[n]
                 cheapest[n] = cost
@@ -80,20 +80,57 @@ function part1(input = read(INPUT_PATH, String))
     length(path) - 1
 end
 
-function part2(input = read(INPUT_PATH, String))
-    map, _, dest = parse_input(strip(input))
-    min_length = typemax(Int)
-    for a_pos ∈ findall(iszero, map)
-        path = a_star(map, manhattan_dist, a_pos, dest)
-        if path === nothing
-            continue
-        end
-        l = length(path) - 1
-        if l < min_length
-            min_length = l
+iszero(map::Array{Int, 2}, p::CartesianIndex) = map[p] == 0
+
+function neighbors_down(map, p::CartesianIndex)
+    steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    ns = CartesianIndex[]
+    for step ∈ steps
+        cand = CartesianIndex(Tuple(p) .+ step)
+        if checkbounds(Bool, map, cand) && map[p] - map[cand] <= 1
+            push!(ns, cand)
         end
     end
-    min_length
+    ns
+end
+
+function path_length(p::CartesianIndex, came_from)
+    n = 0
+    while p !== nothing
+        p = get(came_from, p, nothing)
+        n += 1
+    end
+    n
+end
+
+function bfs(map, start::CartesianIndex, end_cond::Function)
+    q = Queue{CartesianIndex}()
+    visited = Set{CartesianIndex}()
+    from = Dict{CartesianIndex, CartesianIndex}()
+    
+    push!(visited, start)
+    enqueue!(q, start)
+    while !isempty(q)
+        p = dequeue!(q)
+        if end_cond(map, p)
+            return path_length(p, from)
+        end
+        for n ∈ neighbors_down(map, p)
+            if n ∉ visited
+                push!(visited, n)
+                from[n] = p
+                enqueue!(q, n)
+            end
+        end
+    end
+    return nothing
+end
+
+function part2(input = read(INPUT_PATH, String))
+    map, _, dest = parse_input(strip(input))
+    # BFS returns the path inclusive of the start and end square.
+    # We're finding the number of steps.
+    bfs(map, dest, iszero) - 1
 end
 
 end # module Day12
