bf7cca6e24d9f79f5f55e02abe4c8736720e6c5e
Matthew West <mattwest@google.com>
2022-12-13 09:32:17 -0700

    Use Multiple Dispatch for ordered function.

--- src/day13.jl
+++ src/day13.jl
@@ -14,48 +14,50 @@ function parse_input(s)
     data
 end
 
-function ordered((left, right))::Ordered
-    if typeof(left) == Int && typeof(right) == Int
-        if left < right
-            return RIGHT
-        elseif left > right
-            return WRONG
-        else
-            return UNDECIDED
-        end
-    elseif typeof(left) <: Vector && typeof(right) <: Vector
-        for i ∈ 1:min(length(left), length(right))
-            status = ordered((left[i], right[i]))
-            if status != UNDECIDED
-                return status
-            end
-        end
-        if length(left) < length(right)
-            return RIGHT
-        elseif length(left) > length(right)
-            return WRONG
-        else
-            return UNDECIDED
-        end
+function ordered(left::Int, right::Int)::Ordered
+    if left < right
+        return RIGHT
+    elseif left > right
+        return WRONG
     else
-        if typeof(left) == Int
-            return ordered(([left], right))
-        elseif typeof(right) == Int
-            return ordered((left, [right]))
+        return UNDECIDED
+    end
+end
+    
+function ordered(left::Vector, right::Vector)
+    for i ∈ 1:min(length(left), length(right))
+        status = ordered(left[i], right[i])
+        if status != UNDECIDED
+            return status
         end
+    end
+    if length(left) < length(right)
+        return RIGHT
+    elseif length(left) > length(right)
+        return WRONG
+    else
+        return UNDECIDED
+    end
+end
+
+function ordered(left, right)
+    if typeof(left) == Int
+        return ordered([left], right)
+    elseif typeof(right) == Int
+        return ordered(left, [right])
     end
 end
 
 function part1(input = read(INPUT_PATH, String))
     pairs = parse_input(input)
-    sum(findall(p -> ordered(p) == RIGHT, pairs))
+    sum(findall(p -> ordered(p...) == RIGHT, pairs))
 end
 
 function part2(input = read(INPUT_PATH, String))
     pairs = parse_input(input)
     packets = [p[i] for p ∈ pairs for i ∈ 1:2]
     push!(packets, [[2]], [[6]])
-    sort!(packets; lt=(a,b)->ordered((a,b))==RIGHT)
+    sort!(packets; lt=(a,b)->ordered(a,b)==RIGHT)
     div1, div2 = findfirst(p->p==[[2]], packets), findfirst(p->p==[[6]], packets)
     div1 * div2
 end
