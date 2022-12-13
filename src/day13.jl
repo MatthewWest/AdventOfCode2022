08c4305e087512ebdc964123bcebc33e3e358b2c
Matthew West <mattwest@google.com>
2022-12-12 22:57:58 -0700

    Adjust a bit of logic fro day 13.

--- src/day13.jl
+++ src/day13.jl
@@ -2,7 +2,7 @@ module Day13
 
 INPUT_PATH = joinpath(@__DIR__, "../data/day13.txt")
 
-@enum Ordered RIGHT NOT_RIGHT UNDECIDED
+@enum Ordered RIGHT WRONG UNDECIDED
 
 function parse_input(s)
     pairs = split(rstrip(s), "\n\n")
@@ -18,24 +18,24 @@ function ordered((left, right))::Ordered
     if typeof(left) == Int && typeof(right) == Int
         if left < right
             return RIGHT
-        elseif left == right
-            return UNDECIDED
+        elseif left > right
+            return WRONG
         else
-            return NOT_RIGHT
+            return UNDECIDED
         end
     elseif typeof(left) <: Vector && typeof(right) <: Vector
         for i ∈ 1:min(length(left), length(right))
             status = ordered((left[i], right[i]))
-            if status == RIGHT || status == NOT_RIGHT
+            if status != UNDECIDED
                 return status
             end
         end
         if length(left) < length(right)
             return RIGHT
-        elseif length(left) == length(right)
-            return UNDECIDED
+        elseif length(left) > length(right)
+            return WRONG
         else
-            return NOT_RIGHT
+            return UNDECIDED
         end
     else
         if typeof(left) == Int
@@ -53,15 +53,10 @@ end
 
 function part2(input = read(INPUT_PATH, String))
     pairs = parse_input(input)
-    packets = []
-    for pair ∈ pairs
-        push!(packets, pair[1])
-        push!(packets, pair[2])
-    end
-    push!(packets, [[2]])
-    push!(packets, [[6]])
+    packets = [p[i] for p ∈ pairs for i ∈ 1:2]
+    push!(packets, [[2]], [[6]])
     sort!(packets; lt=(a,b)->ordered((a,b))==RIGHT)
-    div1, div2 = findfirst(p->p == [[2]], packets), findfirst(p->p==[[6]], packets)
+    div1, div2 = findfirst(p->p==[[2]], packets), findfirst(p->p==[[6]], packets)
     div1 * div2
 end
 
